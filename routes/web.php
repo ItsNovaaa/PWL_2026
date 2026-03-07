69e3fb3e7524d925c5cd2dd58bb47362e18b3288
Nova <ininopal222@gmail.com>
2026-03-07 21:25:20 +0700

    percobaan 4

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\LevelController;
 
 Route::get('/', function () {
     return view('welcome');
